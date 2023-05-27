ac30270957c5c24855014561cf80de7adb7e2708
unknown <eugeniopb4@gmail.com>
2023-05-27 19:04:39 -0300

    feat: invert sprite to left or right when player is moving

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRb;
     [SerializeField]
     private float moveSpeed;
-    private Vector2 moveInput;
+    private bool isLookLeft; // true = olhando para a esqueda, false = olhando para a direita
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +21,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        playerRb.velocity = moveInput.normalized * moveSpeed;
+        Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        playerRb.velocity = moveDirection.normalized * moveSpeed;
+
+        if(moveDirection.x  > 0 && isLookLeft == true) {
+            Flip();
+        } else if (moveDirection.x < 0 && isLookLeft == false) {
+            Flip();
+        }
+
+
     }
 
+    void Flip() {
+        isLookLeft = !isLookLeft;
+        float x = transform.localScale.x * -1;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
 }
