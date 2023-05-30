3a90539790504aa30cd8c60f8716bf499e7feb92
unknown <eugeniopb4@gmail.com>
2023-05-30 10:53:01 -0300

    feat: moviment of enemies

--- Assets/_Project/Scripts/EnemyAI.cs
+++ Assets/_Project/Scripts/EnemyAI.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     public EnemyData enemyData;
+    private Vector3 target;
+    private bool isLookLeft;
+    private Rigidbody2D _rigidbody;
+    private Vector2 moveDirection;
+
+    private float currentHealth;
+    private float knockBackTime;
+    private int knockBackFactor = 1;
+
+    public GameObject hitArea;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        StartCoroutine(nameof(IETargetUpdate));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _rigidbody.velocity = moveDirection.normalized * enemyData.moveSpeed * knockBackFactor;
+    }
+
+    IEnumerator IETargetUpdate() {
+        while(true) {
+            yield return new WaitForSeconds(enemyData.targetUpdateDelay);
+            target = Core.Instance.gameManager.player.position;
+            moveDirection = target - transform.position;
+        }
     }
 }
--- Assets/_Project/Scripts/Scriptables/EnemyData.cs
+++ Assets/_Project/Scripts/Scriptables/EnemyData.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Scriptable/Enemy", order =3)]
 public class EnemyData : ScriptableObject
 {
-
+    public float targetUpdateDelay;
+    public float moveSpeed;
 }
