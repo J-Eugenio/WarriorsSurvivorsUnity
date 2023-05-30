e7941b9380c8641f456afa819185619055a94a3a
unknown <eugeniopb4@gmail.com>
2023-05-30 10:27:58 -0300

    feat: enemies prefabs and scripts base

---
+++ Assets/_Project/Scripts/EnemyAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public EnemyData enemyData;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
